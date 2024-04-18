589b0c13d0979cbd31fecad834e3349ffcac149d
louie <louie@gmail.com>
2024-04-18 15:17:33 +0800

    rename salary to retail price

--- public/read.php
+++ public/read.php
@@ -79,7 +79,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                         <label>Retail Price</label>
                         <p><b><?php echo $row["product_retail_price"]; ?></b></p>
                     </div>
-                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                    <p><a href="../index.php" class="btn btn-primary">Back</a></p>
                 </div>
             </div>        
         </div>
