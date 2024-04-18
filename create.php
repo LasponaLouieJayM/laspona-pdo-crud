c21521e67a71a1e7d4cbc1f577d09d85e9ff7826
louie <louie@gmail.com>
2024-04-18 14:20:14 +0800

    rename salary to retailprice

--- create.php
+++ create.php
@@ -29,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate salary
     $input_retailprice = trim($_POST["retailprice"]);
     if(empty($input_retailprice)){
-        $salary_err = "Please enter the salary amount.";     
+        $retailprice_err = "Please enter the salary amount.";     
     } elseif(!ctype_digit($input_salary)){
         $salary_err = "Please enter a positive integer value.";
     } else{
