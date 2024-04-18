ac9558282892824dfa4922c09eb0b485cd49a9d6
louie <louie@gmail.com>
2024-04-18 15:29:34 +0800

    changing file path and rename employees to products

--- public/update.php
+++ public/update.php
@@ -1,6 +1,6 @@
 <?php
 // Include config file
-require_once "../db/sconfig.php";
+require_once "../db/config.php";
  
 // Define variables and initialize with empty values
 $name = $address = $salary = "";
@@ -77,7 +77,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         $id =  trim($_GET["id"]);
         
         // Prepare a select statement
-        $sql = "SELECT * FROM employees WHERE id = ?";
+        $sql = "SELECT * FROM products WHERE id = ?";
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "i", $param_id);
